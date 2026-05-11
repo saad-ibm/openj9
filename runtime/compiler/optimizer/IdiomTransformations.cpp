075c4590b8f23f874d98cf3dbaa2ed72b24e9d7c
Saad <saad.younus@ibm.com>
2026-05-11 11:22:01 -0400

    Fix arrayset transformation to prevent merging - take 2

--- runtime/compiler/optimizer/IdiomTransformations.cpp
+++ runtime/compiler/optimizer/IdiomTransformations.cpp
@@ -8722,6 +8722,8 @@ bool CISCTransform2ArraySet(TR_CISCTransformer *trans)
         TR::Node *outerBaseArray = getArrayBase(outerStoreNode);
         
         if (!outerBaseArray) {
+            printf("Reached here arraysetidiomfix - outer array is null\n");
+        
             dumpOptDetails(comp, "Could not extract base array from outer store node %p\n", outerStoreNode);
             return false;
         }
@@ -8736,6 +8738,8 @@ bool CISCTransform2ArraySet(TR_CISCTransformer *trans)
             TR::Node *innerBaseArray = getArrayBase(innerStoreNode);
             
             if (!innerBaseArray) {
+                printf("Reached here arraysetidiomfix - inner array is null\n");
+        
                 dumpOptDetails(comp, "Could not extract base array from inner store node %p\n", innerStoreNode);
                 return false;
             }
